5504bbb3946ecbd7cc67e02070142739eb78d216
secary <secary2010@.com>
2026-07-09 15:45:11 +0800

    Ref: added springboot frame

---
+++ backend/src/test/java/com/janus/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janus.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
